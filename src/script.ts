025c27d1371b3fc6e23f0d642bac0058b8147ef5
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-08-30 11:54:08 +0200

    Typescript example

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,22 @@
 console.log("Hello World");
+
+interface DinosaurData {
+    dinosaur: string;
+    legs: number;
+}
+
+const data: DinosaurData[] = [
+    {
+    dinosaur: 'Triceratops',
+    legs: 4
+    },
+    {
+        dinosaur: 'Tyrannosaurus',
+        legs: 2
+    }
+]
+
+data.forEach(dino => {
+    console.log(dino.dinosaur);
+    
+})
